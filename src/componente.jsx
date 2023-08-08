97fadad4d88b5585fcc61e56428a2a3da977b1cd
gonzalo <gonzalo.troncoso10@hotmail.com>
2023-08-07 23:51:14 -0300

    creacion del proyecto

---
+++ src/componente.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const string = "texto"
+const number = 912
+const array = ["god", "carp"]
+
+
+export const Componente = () => {
+  return (
+    <>
+        <h1>Hola Mundo!!</h1>
+        <div>{string}</div> 
+        <div>{number}</div>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Componente} from './componente'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Componente/>
+  </React.StrictMode>,
+)
